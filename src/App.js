74e302b1e8acf5ba1e31d05caa83908c10dee295
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-04-15 19:16:21 +0900

    setting : image폴더 추가, title명 변경, 폰트 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import PopupDetail from './component/popup/PopupDetail';
+import { BrowserRouter as Router, Route,Routes, Link } from 'react-router-dom';
+import PopupList from './component/popup/PopupList';
 
 function App() {
   return (
-    
   <Router>
   <div className="App">
             <nav>
-                <ul>
-                    <li>
-                        <Link to="/PopupDetail">PopupDetail</Link> {/* 새로운 링크 페이지로 이동하는 링크 */}
-                    </li>
-                </ul>
+              <Link to="/PopupList">Popup</Link> {/* 새로운 링크 페이지로 이동하는 링크 */}
             </nav>
-            <Route path="/PopupDetail" component={PopupDetail} /> {/* 새로운 링크 페이지의 라우트 */}
+            <Routes>
+              <Route path="/PopupList" element={<PopupList />} /> {/* 새로운 링크 페이지의 라우트 */}
+            </Routes>
         </div>
   </Router>
-  
   );
 }
 
