e9f9a30ab9b5454c1f67dfbdde31cc48bafa9fb9
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-05-02 14:01:23 +0900

    refactor: Router 없는 코드 주석 처리

--- src/component/frame/Router.js
+++ src/component/frame/Router.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-//페이지
-import PopupList from "../popup/PopupList";
-import PopupDetail from "../popup/PopupDetail";
+// //페이지
+// import PopupList from "../popup/PopupList";
+// import PopupDetail from "../popup/PopupDetail";
 
 
 const AppRouter = () =>{
     return(
       <Router>
         <Routes>
-            <Route path="PopupList" element={<PopupList />} />
-            <Route path="PopupDetail" element={<PopupDetail />} />
+            {/* <Route path="PopupList" element={<PopupList />} />
+            <Route path="PopupDetail" element={<PopupDetail />} /> */}
         </Routes>
       </Router>
 
