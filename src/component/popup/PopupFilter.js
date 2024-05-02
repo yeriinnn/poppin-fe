38ec444c7bec3c184b6e436127a20bef3eb93a51
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-05-03 04:15:32 +0900

    style: 애니메이션 추가, 전체적인 style 변경

--- src/component/popup/PopupFilter.js
+++ src/component/popup/PopupFilter.js
@@ -4,15 +4,15 @@ import "./css/PopupFilter.css";
 const PopupFilter = () => {
   //-----------------checkBox(카테고리) 처리---------------------
   const category = {
-    fashion: '패션',
-    beauty: '뷰티',
-    food: '음식',
-    celeb: '연예',
-    digital: '가전/디지털',
-    character: '캐릭터',
-    living: '리빙',
-    game: '게임'
-  }
+    fashion: "패션",
+    beauty: "뷰티",
+    food: "음식",
+    celeb: "연예",
+    digital: "가전/디지털",
+    character: "캐릭터",
+    living: "리빙",
+    game: "게임",
+  };
 
   //여러 개의 체크 박스 상태 저장 useState hook 사용
   const [checkboxStates, setCheckBoxStates] = useState({
@@ -25,24 +25,31 @@ const PopupFilter = () => {
     living: false,
     game: false,
   });
-  
+
   //체크 박스 변경 이벤트 핸들러
   const handleCheckboxChange = (event) => {
-    const { name, checked } = event.target;   //이벤트 발생한 요소에 접근
-    
+    const { name, checked } = event.target; //이벤트 발생한 요소에 접근
+
     // checkboxStates상태 변경
-    setCheckBoxStates(prevState => ({       //prevState: 이전 상태 (React Hook에서 제공하는 기능)
+    setCheckBoxStates((prevState) => ({
+      //prevState: 이전 상태 (React Hook에서 제공하는 기능)
       ...prevState,
-      [name]: checked
+      [name]: checked,
     }));
-    console.log(name, ': ', checked);
+    console.log(name, ": ", checked);
   };
 
   //체크박스 렌더링 함수
   const renderCheckBox = () => {
     return Object.keys(checkboxStates).map((key) => (
-      <div className="filter-check">
-        <input type="checkbox" id={key} name={key} checked={checkboxStates[key]} onChange={handleCheckboxChange}/>
+      <div className={`filter-check ${checkboxStates[key] ? "on" : ""}`}>
+        <input
+          type="checkbox"
+          id={key}
+          name={key}
+          checked={checkboxStates[key]}
+          onChange={handleCheckboxChange}
+        />
         <label for={key}>{category[key]}</label>
       </div>
     ));
@@ -50,27 +57,35 @@ const PopupFilter = () => {
 
   //--------------------라디오 버튼(기간) 처리 -------------------
   //선택된 라디오 버튼 상태
-  const period =[
-    {id: 0, name:"all", label: "모든 팝업"},
-    {id: 1, name:"open", label: "현재 진행 중인 팝업만"},
-    {id: 2, name:"close", label: "종료된 팝업만"}
-  ]
+  const period = [
+    { id: 0, name: "all", label: "모든 팝업" },
+    { id: 1, name: "open", label: "현재 진행 중인 팝업" },
+    { id: 2, name: "close", label: "종료된 팝업" },
+  ];
 
   const [selectedRadio, setSelectedRadio] = useState("open");
-  const handleRadioChange = (event) =>{
+  const handleRadioChange = (event) => {
     setSelectedRadio(event.target.id);
-    console.log(event.target.id,': ',event.target.value);
-  }
+    console.log(event.target.id, ": ", event.target.value);
+  };
 
-  const renderRadioButton=()=>{
-    return period.map((period)=>(
-      <div className="filter-check" key={period.id}>
-        <input type="radio" name="period" id={period.name} checked={selectedRadio === period.name} onChange={handleRadioChange}/>
+  const renderRadioButton = () => {
+    return period.map((period) => (
+      <div
+        className={`filter-check ${selectedRadio === period.name ? "on" : ""}`}
+        key={period.id}
+      >
+        <input
+          type="radio"
+          name="period"
+          id={period.name}
+          checked={selectedRadio === period.name}
+          onChange={handleRadioChange}
+        />
         <label for={period.name}>{period.label}</label>
       </div>
     ));
   };
-
 
   //-----------------렌더링------------------
   return (
@@ -86,19 +101,29 @@ const PopupFilter = () => {
           src={require("../../assets/images/arrow.png")}
         />
       </div>
-      
-      <hr/>
+
+      <hr />
 
       <div>
-        <fieldset>
-          <legend>Category</legend>
+        <div className="filter-inner">
+          <div className="filter-location bold">Category
+            <img
+              className="filter-img right"
+              src={require("../../assets/images/up.png")}
+            />
+          </div>
           {renderCheckBox()}
-        </fieldset>
-        <hr/>
-        <fieldset>
-            <legend>Period</legend>
-            {renderRadioButton()}
-          </fieldset>
+        </div>
+        <div className="filter-inner">
+          <div className="filter-location bold">Period
+            <img
+              className="filter-img right"
+              src={require("../../assets/images/up.png")}
+            />
+          </div>
+          {renderRadioButton()}
+        </div>
+        
       </div>
     </div>
   );
