81314720bb71fc07d2db4a2cc5ae4f385b4ba032
Yerin Song <yrsong0915@gmail.com>
2024-05-03 20:48:38 +0900

    add: divider 이미지 추가

--- src/component/frame/Header.js
+++ src/component/frame/Header.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React from 'react';
+import './Header.css';
 
 const Header = () => {
-    return (
-        <Header>
-            <hr/>
-            헤더입니다람쥐
-        </Header>
-    );
+  return (
+    <header className="main-header">
+      <div className="header-content">
+        <img src="/public/images/popin.png" alt="Logo" />
+        <img src="/src/assets/images/divider.png" alt="Divider" />
+        <div className="header-buttons">
+          <button className="header-button">Popup</button>
+          <button className="header-button">Map</button>
+          <button className="header-button">Live</button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
