a898fc7adf17de44086d8505aaf23a2833c61aec
Yerin Song <yrsong0915@gmail.com>
2024-05-03 20:08:46 +0900

    refactor: top->header로 파일명 변경

---
+++ src/component/frame/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <Header>
+            <hr/>
+            헤더입니다람쥐
+        </Header>
+    );
+};
+
+export default Header;
