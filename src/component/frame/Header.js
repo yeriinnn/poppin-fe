228ed89043d9a4c73beb7c9689ce32e6a5f535e6
Yerin Song <yrsong0915@gmail.com>
2024-05-03 22:24:48 +0900

    ui: 헤더 완성 및 popupdetail 페이지에 넣음

--- src/component/frame/Header.js
+++ src/component/frame/Header.js
@@ -5,12 +5,16 @@ const Header = () => {
   return (
     <header className="main-header">
       <div className="header-content">
-        <img src="/public/images/popin.png" alt="Logo" />
-        <img src="/src/assets/images/divider.png" alt="Divider" />
+        <img src="/images/popin.png" alt="Logo" />
+        <img src="/images/divider.png" alt="Divider" />
         <div className="header-buttons">
           <button className="header-button">Popup</button>
           <button className="header-button">Map</button>
           <button className="header-button">Live</button>
+        </div>
+        <div className="header-buttons">
+          <button className="header-button special-button">Sign In</button>
+          <button className="header-button special-button">Join</button>
         </div>
       </div>
     </header>
