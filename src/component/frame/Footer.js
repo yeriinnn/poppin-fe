532dbb2424016a5f934bfc0802613861fc01945b
hyo-limilimee <1001lily@naver.com>
2024-05-04 19:00:20 +0900

    feat: index.js에 헤더 적용

--- src/component/frame/Footer.js
+++ src/component/frame/Footer.js
@@ -2,10 +2,9 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer>
-            <hr/>
-            푸터입니다!
-            <a href="/PopupList">Popup</a>
+        <footer className="main-footer">
+            
+
         </footer>
     );
 };
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Footer from './component/frame/Footer'
+import Header from './component/frame/Header'
 import './GlobalStyle.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
     <Footer />
-    </React.StrictMode>
+  </React.StrictMode>
 );
 
