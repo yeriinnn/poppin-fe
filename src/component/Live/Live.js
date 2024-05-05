5718712094d535e1287cbb17b7724271fd44ac06
hyo-limilimee <1001lily@naver.com>
2024-05-06 00:29:23 +0900

    feat: 검색창 css 수정

--- src/component/Live/Live.js
+++ src/component/Live/Live.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./css/Live.css";
+import LiveBox from "./LiveBox";
+import dbjson from "../popup/popupdb.json";
 
 const Live = () => {
+    const [live, setLive] = useState(null);
+
+    const getLive = async () => {
+        setLive(dbjson.popup);
+    };
+
+    useEffect(() => {
+        setLive(dbjson);
+    }, []);
+
+    if (live == null) {
+        return null;
+    }
+
     return (
-        <div>
-            <div className="LiveList">
+        <div className="live-page">
+              <div className="live-chat-title">
+                    <strong style={{ fontSize: '36px'}}>Live Chat</strong>
+                    <br/><br/>
+                    <strong style={{ fontSize: '24px' }}>관심있는 팝업 스토어의 현장 엿보기 !</strong>
+                </div>
                 {/* search 창 */}
                 <div className="list-top">
                     <span>
@@ -20,9 +40,31 @@ const Live = () => {
                 </div>
                 {/* search 창 */}
 
+            <div className="live-list">
+                {/* LiveList */}
+                <div className="list-container-wrapper">
+                    <div className="list-container">
+                        {live.map((popup) => (
+                            <LiveBox
+                                id={popup.id}
+                                name={popup.name}
+                                start_date={popup.start_date}
+                                end_date={popup.start_date}
+                                image={popup.image}
+                            />
+                        ))}
+
+                        {/* Load more 100+ 버튼 */}
+                        <div className="list-bottom">
+                            <button className="moreBtn">Load more +</button>
+                        </div>
+                        {/* Load more 100+ 버튼 */}
+                    </div>
+                </div>
                 {/* LiveList */}
             </div>
         </div>
     );
 };
+
 export default Live;
--- src/component/Live/LiveBox.js
+++ src/component/Live/LiveBox.js
@@ -1,5 +1,5 @@
 import React from "react";
-import '../Live/css';
+import './css/LiveBox.css';
 import { useNavigate } from "react-router-dom";
 
 const LiveBox = (props) =>{
