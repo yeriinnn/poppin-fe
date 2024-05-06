5ae455e8eff7a65c3b263b424204f63b44884b3c
Yerin Song <yrsong0915@gmail.com>
2024-05-06 14:43:24 +0900

    ui: 메인페이지 이미지 슬라이드 기능 추가

--- src/component/main/main.js
+++ src/component/main/main.js
@@ -1,42 +1,43 @@
-import React, { useEffect } from 'react';
-import './css/Main.css';
-import Header from './Header'; // 헤더 컴포넌트 import
-import Swiper from 'swiper'; // Swiper 라이브러리 import
-import 'swiper/css/swiper.min.css'; // Swiper CSS 파일 import
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+import "swiper/css"; // Swiper의 기본 CSS
+import "swiper/css/navigation"; // Swiper의 Navigation 버튼 CSS
+import "swiper/css/pagination"; // Swiper의 Pagination CSS
+import "./css/Main.css"; // main 컴포넌트의 CSS 파일
+
+SwiperCore.use([Navigation, Pagination, Autoplay]); // Swiper에서 사용할 Navigation, Pagination, Autoplay 기능 활성화
 
 const Main = () => {
-  useEffect(() => {
-    // Swiper 초기화
-    new Swiper('.swiper-container', {
-      loop: true,
-      autoplay: {
-        delay: 5000, // 5초마다 자동 넘김
-      },
-      slidesPerView: 5,
-      spaceBetween: 30,
-    });
-  }, []);
+  const params = {
+    spaceBetween: 10,
+    slidesPerView: "1",
+    loop: true,
+    navigation: true,
+    pagination: {
+      clickable: true,
+    },
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+  };
 
   return (
-    <div>
-      <Header /> 
-      
-      <div className="image-slider">
-        {/* Swiper 컨테이너 */}
-        <div className="swiper-container">
-          <div className="swiper-wrapper">
-            {/* 슬라이드 아이템 예시 */}
-            <div className="swiper-slide">./assets/images/popup1.png</div>
-            <div className="swiper-slide">./assets/images/popup1.png</div>
-            <div className="swiper-slide">./assets/images/popup1.png</div>
-            <div className="swiper-slide">./assets/images/popup1.png</div>
-            <div className="swiper-slide">./assets/images/popup1.png</div>
-          </div>
-        </div>
-      </div>
-
-      <div className="popup-section">
-        진행중인 팝업 TOP 3
+    <div className="main-container">
+      {/* Swiper를 사용하여 이미지 슬라이드 구현 */}
+      <div className="swiper-container">
+        <Swiper {...params}>
+          <SwiperSlide>
+            <img src="/src/assets/images/popup1.png" alt="Slide 1" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/src/assets/images/popup2.jpg" alt="Slide 2" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/src/assets/images/popup2.jpg" alt="Slide 3" />
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
   );
