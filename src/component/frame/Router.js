d7f99f000c1ff61752afd80c502effaad1620237
hyo-limilimee <1001lily@naver.com>
2024-05-06 14:02:25 +0900

    refactor: 패키지 명 변경

--- src/component/frame/Router.js
+++ src/component/frame/Router.js
@@ -5,9 +5,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PopupList from "../popup/PopupList";
 import PopupDetail from "../popup/PopupDetail";
 import Signup from "../myPage/Signup";
-import Map from "../Map/Map"
-import KakaoMap from "../Map/KakaoMap";
-import Live from "../Live/Live";
+import Map from "../map/Map"
+import KakaoMap from "../map/KakaoMap";
+import Live from "../live/Live";
 
 const AppRouter = () =>{
     return(
