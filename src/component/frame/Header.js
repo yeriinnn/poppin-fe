216f84d93925fbda0ccd176d8836fc058dc467a2
Yerin Song <yrsong0915@gmail.com>
2024-05-06 15:13:14 +0900

    feat: 헤더의 로고, 팝업, 맵,라이브 클릭시 알맞은 페이지로 이동하는 기능 추가

--- src/component/frame/Header.js
+++ src/component/frame/Header.js
@@ -2,15 +2,26 @@ import React from 'react';
 import './css/Header.css';
 
 const Header = () => {
+  const handleLogoClick = () => {
+    window.location.href = '/main'; // 현재 창의 위치를 '/main'으로 변경하여 페이지 이동
+  };
+
   return (
     <header className="main-header">
       <div className="header-content">
-        <img src="/images/popin.png" alt="Logo" />
+        {/* 이미지를 클릭할 때 handleLogoClick 함수 실행 */}
+        <img
+          src="/images/popin.png"
+          alt="Logo"
+          onClick={handleLogoClick}
+          style={{ cursor: 'pointer' }} // 커서 스타일을 포인터로 변경하여 클릭 가능하도록 함
+        />
         <img src="/images/divider.png" alt="Divider" />
         <div className="header-buttons">
-          <button className="header-button">Popup</button>
-          <button className="header-button">Map</button>
-          <button className="header-button">Live</button>
+          {/* 다른 버튼들은 <a> 태그를 사용하여 각각의 경로로 이동 */}
+          <a href="/popup" className="header-button">Popup</a>
+          <a href="/map" className="header-button">Map</a>
+          <a href="/live" className="header-button">Live</a>
         </div>
         <div className="header-buttons">
           <button className="header-button special-button"><strong>Sign In</strong></button>
