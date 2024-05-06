5362ec0ffd3ec6edef16c1fb6e45c9a738dd2bd7
hyo-limilimee <1001lily@naver.com>
2024-05-06 20:38:15 +0900

    fix: 클래스 명 동일 오류 수정

--- src/component/Live/LiveBox.js
+++ src/component/Live/LiveBox.js
@@ -11,19 +11,19 @@ const LiveBox = (props) => {
 
     return (
         <div className="list-box">
-            <img className="box-img" src={require('../../assets/images/' + props.image + '.png')} />
-            <div className="box-content">
-                <div className="box-basic-info">
-                    <div className="box-name">{props.name}</div>
-                    <div className="box-date"> {props.start_date}-{props.end_date}</div>
-                    <div className="box-address">{props.city} {props.local} {props.location}</div>
+            <img className="chat-box-img" src={require('../../assets/images/' + props.image + '.png')} />
+            <div className="chat-box-content">
+                <div className="chat-box-basic-info">
+                    <div className="chat-box-name">{props.name}</div>
+                    <div className="chat-box-date"> {props.start_date}-{props.end_date}</div>
+                    <div className="chat-box-address">{props.city} {props.local} {props.location}</div>
                 </div>
-                <div className="box-join-info">
-                    <div className="box-liked-content">
-                    <img className="box-ic-liked" src={require('../../assets/images/ic_person.png')} />
-                        <div className="box-liked">100명이 와글와글</div>
+                <div className="chat-box-join-info">
+                    <div className="chat-box-liked-content">
+                    <img className="chat-box-ic-liked" src={require('../../assets/images/ic_person.png')} />
+                        <div className="chat-box-liked">100명이 와글와글</div>
                     </div>
-                    <div className="box-point">Ⓒ100P</div>
+                    <div className="chat-box-point">Ⓒ100P</div>
                     <button className="join-button"><strong>join</strong></button>
                 </div>
             </div>
