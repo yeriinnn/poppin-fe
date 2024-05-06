05d8d12f81d7abcd7e018d586a0493f5c5584e46
hyo-limilimee <1001lily@naver.com>
2024-05-06 23:22:43 +0900

    feat: 채팅 목록 박스 배치 완료

---
+++ src/component/Live/ChatBox.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './css/ChatBox.css';
+
+const ChatBox = () => {
+    return (
+        <div className="chat-message-list">
+            <div className="chat-message-box">
+                <div className="blue-rectangle">
+                    
+                </div>
+            </div>
+            <div className="chat-new-box">
+            </div>
+        </div>
+    );
+};
+
+export default ChatBox;
