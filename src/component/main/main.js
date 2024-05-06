843b9de44934fef7b5cb5df828344f9b6ea1d3fc
Yerin Song <yrsong0915@gmail.com>
2024-05-06 14:44:09 +0900

    remove: 주석제거

--- src/component/main/main.js
+++ src/component/main/main.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
-import "swiper/css"; // Swiper의 기본 CSS
-import "swiper/css/navigation"; // Swiper의 Navigation 버튼 CSS
-import "swiper/css/pagination"; // Swiper의 Pagination CSS
-import "./css/Main.css"; // main 컴포넌트의 CSS 파일
+import "swiper/css"; 
+import "swiper/css/navigation"; 
+import "swiper/css/pagination"; 
+import "./css/Main.css";
 
-SwiperCore.use([Navigation, Pagination, Autoplay]); // Swiper에서 사용할 Navigation, Pagination, Autoplay 기능 활성화
+SwiperCore.use([Navigation, Pagination, Autoplay]); 
 
 const Main = () => {
   const params = {
@@ -25,7 +25,6 @@ const Main = () => {
 
   return (
     <div className="main-container">
-      {/* Swiper를 사용하여 이미지 슬라이드 구현 */}
       <div className="swiper-container">
         <Swiper {...params}>
           <SwiperSlide>
