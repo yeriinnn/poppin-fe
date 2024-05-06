b498018660d6dc79bb8cb8b934045317ae7870d7
hyo-limilimee <1001lily@naver.com>
2024-05-06 23:42:03 +0900

    feat: 메시지 박스 css 변경 및 애니메이션 추가

--- src/component/Live/ChatBox.js
+++ src/component/Live/ChatBox.js
@@ -6,11 +6,18 @@ const ChatBox = () => {
         <div className="chat-message-list">
             <div className="chat-message-box">
                 <div className="blue-rectangle">
-                    
+                    <div className="white-inner-rectangle">
+
+                    </div>
                 </div>
             </div>
             <div className="chat-new-box">
+            <div className="blue-rectangle">
+                    <div className="white-inner-rectangle">
+                        
+                    </div>
             </div>
+        </div>
         </div>
     );
 };
