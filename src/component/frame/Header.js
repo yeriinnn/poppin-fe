b2721feb7c00c7495efa820b1003b623acf1fb42
Yerin Song <yrsong0915@gmail.com>
2024-05-06 15:13:56 +0900

    remove: 주석삭제

--- src/component/frame/Header.js
+++ src/component/frame/Header.js
@@ -3,22 +3,20 @@ import './css/Header.css';
 
 const Header = () => {
   const handleLogoClick = () => {
-    window.location.href = '/main'; // 현재 창의 위치를 '/main'으로 변경하여 페이지 이동
+    window.location.href = '/main'; 
   };
 
   return (
     <header className="main-header">
       <div className="header-content">
-        {/* 이미지를 클릭할 때 handleLogoClick 함수 실행 */}
         <img
           src="/images/popin.png"
           alt="Logo"
           onClick={handleLogoClick}
-          style={{ cursor: 'pointer' }} // 커서 스타일을 포인터로 변경하여 클릭 가능하도록 함
+          style={{ cursor: 'pointer' }} 
         />
         <img src="/images/divider.png" alt="Divider" />
         <div className="header-buttons">
-          {/* 다른 버튼들은 <a> 태그를 사용하여 각각의 경로로 이동 */}
           <a href="/popup" className="header-button">Popup</a>
           <a href="/map" className="header-button">Map</a>
           <a href="/live" className="header-button">Live</a>
