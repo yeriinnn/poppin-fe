ecada54a4bdfb6da22888f5c9dbebfc36b84ac2c
hyo-limilimee <1001lily@naver.com>
2024-05-07 00:01:54 +0900

    feat: 채팅 진입 페이지 화면 구현 완료

--- src/component/Live/ChatBox.js
+++ src/component/Live/ChatBox.js
@@ -7,17 +7,25 @@ const ChatBox = () => {
             <div className="chat-message-box">
                 <div className="blue-rectangle">
                     <div className="white-inner-rectangle">
-
+                        <img className="message-image" src={require('../../assets/images/ic_message.png')}
+                            alt="message-image" />
+                        <div className="user-popup-title">
+                            해리포터 팝업스토어
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="chat-new-box">
-            <div className="blue-rectangle">
+                <div className="blue-rectangle">
                     <div className="white-inner-rectangle">
-                        
+                        <img className="new-message-image" src={require('../../assets/images/ic_new_message.png')}
+                            alt="new-message-image" />
+                        <div className="user-popup-title">
+                            눈사람 눙눙이 팝업스토어
+                        </div>
                     </div>
+                </div>
             </div>
-        </div>
         </div>
     );
 };
