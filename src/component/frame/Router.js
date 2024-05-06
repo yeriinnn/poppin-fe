113e7b816d12b7ea4e0a5007ead85bafd028c37e
Yerin Song <yrsong0915@gmail.com>
2024-05-06 13:35:58 +0900

    fix: 폴더명으로 인한 경로 수정

--- src/component/frame/Router.js
+++ src/component/frame/Router.js
@@ -5,8 +5,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PopupList from "../popup/PopupList";
 import PopupDetail from "../popup/PopupDetail";
 import Signup from "../myPage/Signup";
-import Map from "../Map/Map"
-import KakaoMap from "../Map/KakaoMap";
+import Map from "../map/Map"
+import KakaoMap from "../map/KakaoMap";
 
 const AppRouter = () =>{
     return(
