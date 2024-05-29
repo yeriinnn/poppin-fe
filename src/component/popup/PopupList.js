cca28929f139afa5f932f0ac61970c90814f1f7e
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-05-30 04:12:10 +0900

    feat: 리스트 api 연결

--- src/component/popup/PopupList.js
+++ src/component/popup/PopupList.js
@@ -3,23 +3,59 @@ import "./css/PopupList.css";
 import PopupBox from "./PopupBox";
 import PopupFilter from "./PopupFilter";
 import dbjson from "./popupdb.json"; //임시 데이터
-import Header from '../frame/Header'
+import axios from "axios";
 
 const PopupList = () => {
   const [popup, setPopup] = useState(null);
+  const [searchValue, setSearchValue] = useState(""); // 검색 값 상태
+  const [filterValues, setFilterValues] = useState({
+    category: {},
+    period: ""
+  });
 
-  const getPopup = async () => {
-    setPopup(dbjson.popup);
+
+  //팝업 가져오는 함수
+  const getPopupAPI = async () => {
+    try {
+      const requestData = {
+        categories :
+          Object.keys(filterValues.category)
+          .filter(key => filterValues.category[key])
+          .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(true)}`)
+          .join('&'),
+        period: filterValues.period,
+        search: searchValue,
+      };
+  
+      const url = `http://localhost:8080/v1/popup/popuplist?${requestData.categories}&period=${requestData.period}&search=${requestData.search}`;
+      const response = await axios.get(url);
+      setPopup(response.data.data);
+      console.log(response.data.data);
+    } catch (error) {
+      console.error("Error fetching popup detail!!!!:", error);
+    }
   };
+  
 
   //필터링 값 변경시 마다 호출되는 콜 백 함수
   const handleFilterChange = (newFilterValues) => {
-    console.log("newFilterValues", newFilterValues);
+    console.log("newFilterValues", newFilterValues.selectedRadio);
+    setFilterValues({
+      category: newFilterValues.checkboxStates,
+      period: newFilterValues.selectedRadio
+    });
+    getPopupAPI();
   };
 
   useEffect(() => {
     setPopup(dbjson);
   }, []); //종속성 배열
+
+  useEffect(() => {
+    console.log("category", filterValues.category);
+    console.log("period", filterValues.period);
+    getPopupAPI();
+  }, [filterValues]);
 
   if (popup == null) {
     return null;
@@ -44,12 +80,11 @@ const PopupList = () => {
           <div className="list-container">
             {popup.map((popup) => (
               <PopupBox
-                id={popup.id}
-                name={popup.name}
-                start_date={popup.start_date}
-                end_date={popup.start_date}
-                image={popup.image}
-                heart={popup.heart}
+                id={popup.popupId}
+                name={popup.popupName}
+                period ={popup.popupPeriod}
+                image={popup.popupImage}
+                heart={popup.likeCount}
               />
             ))}
             <div className="list-bottom">
