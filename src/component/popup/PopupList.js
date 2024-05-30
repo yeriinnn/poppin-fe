f285ebc3ba99385342fd1591abafab0ab32f8e52
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-05-30 18:50:12 +0900

    refactor: 카테고리 필터링 동작 성공

--- src/component/popup/PopupList.js
+++ src/component/popup/PopupList.js
@@ -13,15 +13,13 @@ const PopupList = () => {
     period: "",
   });
 
+
   //팝업 가져오는 함수
   const getPopupAPI = async () => {
     try {
       const requestData = {
         categories: Object.keys(filterValues.category)
-          .filter((key) => filterValues.category[key])
-          .map(
-            (key) => `${encodeURIComponent(key)}=${encodeURIComponent(true)}`
-          )
+          .map((key) =>`${encodeURIComponent(key)}=${encodeURIComponent(filterValues.category[key])}`)
           .join("&"),
         period: filterValues.period,
         search: searchValue,
@@ -30,11 +28,12 @@ const PopupList = () => {
       const url = `http://localhost:8080/v1/popup/popuplist?${requestData.categories}&period=${requestData.period}&search=${requestData.search}`;
       const response = await axios.get(url);
       setPopup(response.data.data);
-      console.log(response.data.data);
+      //console.log(response.data.data);
     } catch (error) {
       console.error("Error fetching popup detail!!!!:", error);
     }
   };
+
 
   //필터링 값 변경시 마다 호출되는 콜 백 함수
   const handleFilterChange = (newFilterValues) => {
@@ -52,7 +51,6 @@ const PopupList = () => {
 
   useEffect(() => {
     console.log("category", filterValues.category);
-    console.log("period", filterValues.period);
     getPopupAPI();
   }, [filterValues]);
 
@@ -96,8 +94,8 @@ const PopupList = () => {
                 <button className="moreBtn">Load more +</button>
               </div>
             </div>
-          ) :(
-            <div>No Data</div>
+          ) : (
+            <div></div>
           )}
           <div className="list-right right">
             <PopupFilter onFilterChange={handleFilterChange} />
