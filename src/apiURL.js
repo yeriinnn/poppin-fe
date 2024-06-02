9907b73f79395f9ef89830d39b8a6f4e7cd908c1
Yerin Song <yrsong0915@gmail.com>
2024-06-02 11:11:50 +0900

    refactor: 메인 api 엔드포인트 변경

--- src/apiURL.js
+++ src/apiURL.js
@@ -1,6 +1,6 @@
 const apiURLs = {
-    popup: "http://52.79.241.223:8080/v1/popup",
-    chat: "http://52.79.241.223:8080/v1/chat",
+    popup: "http://www.plick.shop/v1/popup",
+    chat: "http://www.plick.shop/v1/chat",
     // 다른 링크들도 필요에 따라 추가 가능
 };
 
